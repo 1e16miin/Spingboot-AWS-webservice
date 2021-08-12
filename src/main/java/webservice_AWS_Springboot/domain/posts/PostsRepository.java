f800462a8ce590d0d7616141c2d180a3be955041
1e16miin <ddogi1513@naver.com>
2021-08-12 23:35:31 +0900

    add jpa

---
+++ src/main/java/webservice_AWS_Springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package webservice_AWS_Springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
